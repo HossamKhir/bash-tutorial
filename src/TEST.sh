1f81b9c5e56bff015f4c84e0e9d7fdc9b5e4fb11
HossamKhir <hossam.khir1@outlook.com>
2020-03-05 18:02:25 +0200

    finished test; starting case

--- src/TEST.sh
+++ src/TEST.sh
@@ -15,12 +15,24 @@ ls -l /usr/bin/[
 ls -l /usr/bin/test 
 
 # bash uses command line arguments as comparing operators
-# -eq : ==
-# -lt : <
-# -gt : >
-# -le : <=
-# -ge : >=
-# -ne : !=
+# integer comparisons
+#   -eq : ==
+#   -lt : <
+#   -gt : >
+#   -le : <=
+#   -ge : >=
+#   -ne : !=
+# other non-numeric comparisons
+#   -nt : newer than
+#   -ot : older than
+#   -ef : equal file path?
+#   -f  : file path exist?
+#   -X  : file executable?
+#   -n  : non-zero length variable?
+#   -a  : file exist?
+#   -e  : file exist?
+#   -S  : file is socket?
+#   -O  : owned by user
 
 # conditionals
 if [ "$X" -lt "0" ]
@@ -37,6 +49,10 @@ fi
 # it can tell in a script that a certain line is spanned onto the next
 [ $X -le "0" ] && \
     echo "X is less than or equal to 0"
+# the '&&' is used to combine commands
+# [ test_condition ] && <true_command> || <false_command>
+# | is used for piping, sending the output of the last command to the next one
+# command0 | command1
 [ "$X" -ge "0" ] && \
       echo "X is more than or equal to zero"
 # within the square brackets "[]", the '=' operator is in fact equality operator, not assignment
@@ -57,3 +73,39 @@ fi
       echo "X is the path of an executable file"
 [ "$X" -nt "/etc/passwd" ] && \
       echo "X is a file which is newer than /etc/passwd"
+
+# using if...elif...else... is preferred for readability
+# using [...] is for while loops & trivial checks 
+
+# to avoid some errors like wrong data types, some checks must be done before operating on variables
+echo -en "Please guess the magic number: "
+read X
+echo $X | grep "[^0-9]" > /dev/null 2>&1
+# grep is a command used for search
+# grep [0-9]:   looks for all lines that might contain the digits form 0 to 9 & other characters as well
+# grep [^0-9]:  looks for lines that does not contain numbers
+# > /dev/null:  directs any o/p or errors to the null device instead of the terminal
+if [ "$?" -eq "0" ]; then
+  # If the grep found something other than 0-9
+  # then it's not an integer.
+  echo "Sorry, wanted a number"
+else
+  # The grep found only 0-9, so it's an integer. 
+  # We can safely do a test on it.
+  if [ "$X" -eq "7" ]; then
+    echo "You entered the magic number!"
+  fi
+fi
+
+# using test with while loops
+X=0
+while [ -n "$X" ]
+do
+    echo "Enter some text (RETURN to quit)"
+    read X
+    # the next if is to avoid untidy ending of the loop
+    if [ -n "$X" ]
+    then
+        echo "You said: $X"
+    fi
+done
